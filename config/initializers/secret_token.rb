fb81aa84324bcd710c81ebff273ab99fd2b1f219
rigibun <mukojin.yoing@gmail.com>
2014-11-14 01:07:49 +0900

    Fix secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -15,8 +15,10 @@ require 'securerandom'
 def secure_token
   token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
-    File.read(token_file.chomp
+    # Use the existing token.
+    File.read(token_file).chomp
   else
+    # Generate a new token and store it in token_file.
     token = SecureRandom.hex(64)
     File.write(token_file, token)
     token
